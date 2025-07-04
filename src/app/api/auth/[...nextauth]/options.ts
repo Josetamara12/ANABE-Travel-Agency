c2f91521607f141bf47a6f29c0af70820de076a4
Josetamara12 <jose.tamara426@pascualbravo.edu.co>
2025-07-04 16:07:21 -0500

    feat: se añade autenticacion de google para el login o register de la app

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,12 +1,17 @@
 import  type { NextAuthOptions } from "next-auth";
-import Githubprovider from "next-auth/providers/github";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProviders from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
   providers: [
-    Githubprovider({
+    GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENTE_SECRET as string,
     }),
     CredentialsProviders({
       name: "Credentials",
@@ -25,12 +30,30 @@ export const options: NextAuthOptions = {
       async authorize(credentials) {
         const user = { id: "29", name: "Jose Tamara", password: "nextauth" }
 
-        if (credentials?.username === user.name && credentials?.password === user.password) {
+        if (credentials?.username === user.name && 
+            credentials?.password === user.password
+          ) {
           return user;
         } else {
           return null;
         }
-      }
-    })
+      },
+    }),
   ],
+  pages: {
+    signIn: "/auth/signin",
+},
+session: {
+  strategy: "jwt",
+},
+callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.user = user
+      return token
+    },
+    async session({ session, token }) {
+      session.user = token.user as any
+      return session
+    },
+  },
 }
